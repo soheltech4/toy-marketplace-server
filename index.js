3f06d9a9453d5e641cdcf2d71fca88b80e44ca15
sohelranagtc <sohelranagtc@gmail.com>
2023-05-21 23:18:27 +0600

    add something

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    
+
     const servicesCollection = client.db('ToyWorld').collection('services')
     const toysCollection = client.db('ToyWorld').collection('toys')
 
@@ -46,26 +46,56 @@ async function run() {
 
 
     // toys
-    app.get('/toys', async(req, res)=>{
+    app.get('/toys', async (req, res) => {
       let query = {}
-      if(req.query?.email){
-        query = {email : req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
       const result = await toysCollection.find(query).toArray();
       res.send(result)
     })
 
 
-    app.post('/toys', async(req, res)=>{
-      const toys= req.body
+    app.post('/toys', async (req, res) => {
+      const toys = req.body
       console.log(toys)
       const result = await toysCollection.insertOne(toys)
       res.send(result)
     })
 
-    app.delete('/toys/:id', async(req, res)=>{
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.findOne(query)
+      res.send(result)
+    })
+
+    const corsOptions = {
+      origin: '*',
+      credentials: true,
+      optionSuccessStatus: 200,
+    }
+    app.use(cors(corsOptions))
+
+    app.put('/toys/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateToy = req.body
+      const toys = {
+        $set: {
+          price: updateToy.price,
+          quantity: updateToy.quantity,
+          description: updateToy.description
+        }
+      }
+      const result = await toysCollection.updateOne(filter, toys, options)
+      res.send(result)
+    })
+
+    app.delete('/toys/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await toysCollection.deleteOne(query)
       res.send(result)
     })
